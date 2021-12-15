449a646f84e82f8025b7c3bca83e5c5e2c21ae43
IlichevDanila <you-may@mail.ru>
2021-12-15 21:02:08 +0300

    Module 3 done

--- src/main/java/diasoft/dilichev/tutorial/GreetingController.java
+++ src/main/java/diasoft/dilichev/tutorial/GreetingController.java
@@ -1,16 +1,17 @@
 package diasoft.dilichev.tutorial;
 
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+
+    private Sequence seq = new Sequence();
 
     @LogRequest
     @GetMapping("/greeting")
@@ -22,5 +23,69 @@ public class GreetingController {
     @GetMapping("/primes")
     public Primes primes(@RequestParam(value = "count", defaultValue = "10") int count) {
         return new Primes(counter.incrementAndGet(), count);
+    }
+
+    @GetMapping("/sequence")
+    public Sequence getSeq(@RequestParam(value = "count", defaultValue = "-1") int count)
+    {
+        //Return all the sequence
+        if(count < 0)
+        {
+            return seq;
+        }
+        if(count > seq.getCount())
+        {
+            throw new NotFoundException();
+        }
+
+        //Return only count elements
+        return seq.getSubsequence(count);
+    }
+
+    @PostMapping("/sequence")
+    public Sequence updateSeq(@RequestBody String SCount)
+    {
+        int count = Integer.parseInt(SCount);
+
+        System.out.println("Post " + count);
+
+        //Add one number
+        if(count < 0)
+        {
+            count = seq.getCount();
+        }
+
+        //Add numbers until we got enough
+        while(seq.getCount() <= count)
+        {
+            seq.add();
+        }
+
+        return seq;
+    }
+
+    @PutMapping("/sequence/{idx}")
+    public Sequence changeSeq(@PathVariable int idx,
+                              @RequestBody String Sval)
+    {
+        int val = Integer.parseInt(Sval);
+        System.out.println("Put " + val);
+        //Update last value
+        if(idx < 0)
+        {
+            idx = seq.getCount() - 1;
+        }
+
+        seq.change(idx, val);
+        return seq;
+    }
+
+    @DeleteMapping("/sequence/{idx}")
+    public Sequence deleteSeq(@PathVariable int idx)
+    {
+        System.out.println("Delete " + idx);
+        seq.delete(idx);
+
+        return seq;
     }
 }
